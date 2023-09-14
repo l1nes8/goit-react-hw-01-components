5613452fd03524fafb7a656c1d7916a7fff0697c
l1nes8 <egor1glavatyje@gmail.com>
2023-09-14 21:03:04 +0300

    update

---
+++ src/components/statistics.js
@@ -0,0 +1,28 @@
+const getRandomColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className="statistics">
+      {title && <h2 className="title">{title}</h2>}
+      <ul className="stat-list">
+        {stats.map(stat => (
+          <li
+            className="item"
+            key={stat.id}
+            style={{ backgroundColor: getRandomColor() }}
+          >
+            <span className="label">{stat.label}</span>
+            <span className="percentage">{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
